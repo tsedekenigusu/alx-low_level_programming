6fb12ac73713a3b6718f9220e37b80684f46f5da
Tsedeke Nigusu <tsedekenigusu339@example.com>
2021-02-03 16:39:54 +0300

    update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-
+/**
+ * first c program 
+ * main - Function that print a text
+ * Return: Always 0
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
