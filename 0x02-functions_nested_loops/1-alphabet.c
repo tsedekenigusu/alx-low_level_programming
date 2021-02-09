b6bbd72a6e46cfca4a4e54c5013ffcc63f66ffed
Tsedeke Nigusu <tsedekenigusu339@example.com>
2021-02-09 21:10:35 +0300

    c program for alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - print_alphabet
+ *
+ * Return:void
+ */
+void print_alphabet(void)
+{
+int x;
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
