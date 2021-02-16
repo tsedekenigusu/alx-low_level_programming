1d5ba3a2511ea9b5abcef8ce8d094eba618e536d
tsedeke nigusu <tsedekenigusu339@gmail.com>
2021-02-16 20:56:08 +0300

    script for puts_half added

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <string.h>
+
+/**
+ * puts_half - check the code for Holberton School students.
+ * @str: variable.
+ */
+void puts_half(char *str)
+{
+  int i = 0;
+  int a;
+  while (str[i] != '\0')
+    {
+      i++;
+    }
+  if (i % 2 == 1)
+    {
+      a = (i + 1) / 2;
+      while (str[a] != '\0')
+	{
+	  _putchar(str[a]);
+	  a++;
+	}
+    }
+
+  else
+    {
+      a = (i / 2);
+      while (str[a] != '\0')
+	{
+	  _putchar(str[a]);
+	  a++;
+	}
+    }
+
+  _putchar('\n');
+}
