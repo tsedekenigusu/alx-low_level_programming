9364e43f289ae26eb84a04aa616cf526aed2cc1a
Tsedeke Nigusu <tsedekenigusu339@example.com>
2021-02-17 23:27:23 +0300

    update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,11 @@
 #include "holberton.h"
-/**
- *reset_to_98 - main function
- *@n : our pointer
- */
 
+/**
+ * reset_to_98 - changes the value of the variable
+ * that's pointed to by the argument to 98
+ *@n: pointer variable
+ */
 void reset_to_98(int *n)
 {
-  *n = 98;
+	*n = 98;
 }
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,5 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -9,4 +12,8 @@ void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
-#endif
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+
+#endif /* HOLBERTON_H */
