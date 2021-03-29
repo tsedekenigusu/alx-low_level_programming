c84dcc61cdc0d256e9ce3216f8f2fde0f806fbd9
Tsedeke Nigusu <tsedekenigusu339@gmail.com>
2021-03-29 22:19:34 +0300

    update

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -5,24 +5,11 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <elf.h>
-
+#include <stdio.h>
 int _putchar(char *c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 char *create_buffer(char *file);
 void close_file(int fd);
-int _putchar(char c);
-void print_magic(unsigned char *e_ident);
-void print_class(unsigned char *e_ident);
-void print_data(unsigned char *e_ident);
-void print_version(unsigned char *e_ident);
-void print_osabi(unsigned char *e_ident);
-void print_type(unsigned int e_type, unsigned char *e_ident);
-void validate_elf(unsigned char *e_ident);
-unsigned int lit_to_big_endian(unsigned int x);
-void print_entry(unsigned int e_type, unsigned char *e_ident);
 #endif
